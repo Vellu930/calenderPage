09423e84cdf6e4a6cc3c1f7575f6e5ded532c29e
vellu930 <martinapanchal33@gmail.com>
2021-04-05 11:23:34 +0200

    partial work on data storage

--- app/src/main/java/com/example/learningdatabaseapp/AddNoteActivity.java
+++ app/src/main/java/com/example/learningdatabaseapp/AddNoteActivity.java
@@ -2,14 +2,74 @@ package com.example.learningdatabaseapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class AddNoteActivity extends AppCompatActivity {
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+
+public class AddNoteActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private static final String FILE_NAME = "notesData.txt";
+    private static EditText titleText, noteText;
+    private static Button saveButton, cancelButton;
+
+    public HashMap<String, String> notesList = new HashMap<String, String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_note);
 
+        titleText = (EditText) findViewById(R.id.noteTitle);
+        noteText = (EditText) findViewById(R.id.noteDesc);
+        saveButton = (Button) findViewById(R.id.submitNewNote);
+        cancelButton = (Button) findViewById(R.id.cancelNewNote);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+    switch (v.getId()) {
+        case R.id.submitNewNote:
+            saveNote();
+            startActivity(new Intent(this, ViewNoteActivity.class));
+                break;
+        case R.id.cancelNewNote:
+            startActivity(new Intent(this, MainActivity.class));
+                break;
+        }
+    }
+
+    private void saveNote() {
+        String title = titleText.getText().toString();
+        String noteDesc = noteText.getText().toString();
+        notesList.put(title, noteDesc);
+        FileOutputStream fos = null;
+        try {
+            fos = openFileOutput(FILE_NAME, MODE_PRIVATE);
+            fos.write(title.getBytes());
+            fos.write(noteDesc.getBytes());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/learningdatabaseapp/MainActivity.java
+++ app/src/main/java/com/example/learningdatabaseapp/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.learningdatabaseapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.TextView;
@@ -12,7 +14,7 @@ import android.widget.TextView;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private TextView showDateTxt;
     private Button editNoteButton;
@@ -23,20 +25,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         addComponents();
         displayCalenderDots();
+
+        editNoteButton.setOnClickListener(this);
 
         Date today = new Date();
         SimpleDateFormat formatting = new SimpleDateFormat("EEE \n dd-MMM-yyyy");
         String strDateNow = formatting.format(today);
         showDateTxt.setText(strDateNow.toString());
     }
+    @Override
+    public void onClick(View v) {
+     //Change this to switch if need more buttons
+        if (v.getId() == R.id.editNoteButton) {
+            startActivity(new Intent(this, AddNoteActivity.class));
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflator = getMenuInflater();
-        inflator.inflate(R.menu.my_menu, menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.my_menu, menu);
         return true;
     }
 
@@ -49,4 +59,6 @@ public class MainActivity extends AppCompatActivity {
         editNoteButton = findViewById(R.id.editNoteButton);
         calendar = findViewById(R.id.calendarView);
     }
+
+
 }
